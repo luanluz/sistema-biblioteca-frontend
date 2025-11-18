e56d7256f544b613adfa296f4f624474ac22b831
Luan Luz <luanbrunoalvesluz@gmail.com>
2025-11-18 02:50:47 -0300

    feat(routing): atualiza rotas e navegação
    - remove rota de início.
    - adiciona rotas para as páginas de livros, autores e assuntos.
    - implementa navegação no menu principal para as novas páginas.
    - remove componentes da página de início.
    
    closes #4, #5, #6, #7

--- src/app/features/features.routes.ts
+++ src/app/features/features.routes.ts
@@ -2,13 +2,23 @@ import { Routes } from '@angular/router';
 
 export const featuresRoutes: Routes = [
     {
-        path: 'inicio',
-        loadComponent: () => import('@features/inicio/inicio.page').then((m) => m.InicioPage),
-        title: 'Início',
+        path: '',
+        redirectTo: 'livros',
+        pathMatch: 'full',
     },
     {
-        path: '',
-        redirectTo: 'inicio',
-        pathMatch: 'full',
+        path: 'livros',
+        loadComponent: () => import('@features/livro/livro.page').then((m) => m.LivroPage),
+        title: 'Livros',
+    },
+    {
+        path: 'autores',
+        loadComponent: () => import('@features/autor/autor.page').then((m) => m.AutorPage),
+        title: 'Autores',
+    },
+    {
+        path: 'assuntos',
+        loadComponent: () => import('@features/assunto/assunto.page').then((m) => m.AssuntoPage),
+        title: 'Assuntos',
     },
 ];
